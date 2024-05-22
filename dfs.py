defebb0b3b0779477d45304ff96cc3db28514d87
xxxmoney <xxxmoney111@gmail.com>
2024-05-22 12:04:05 +0200

    Smol fix

--- dfs.py
+++ dfs.py
@@ -12,10 +12,14 @@ class DepthFirstSearch:
     def __init__(self, graph, order=None):
         self.graph = graph
         self.states = {vertex: VertexState.UNVISITED for vertex in graph}
+        self.time = 0
+        self.times = {vertex: (0, 0) for vertex in graph}
         self.__events = Events()
         self.order = order if order else [vertex for vertex in graph]
 
     def run(self):
+        self.__call_on_change()
+
         for vertex in self.order:
             if self.states[vertex] == VertexState.UNVISITED:
                 self.visit(vertex)
@@ -24,6 +28,9 @@ class DepthFirstSearch:
 
     def visit(self, vertex):
         self.states[vertex] = VertexState.VISITING
+        self.time += 1
+        self.times[vertex] = (self.time, 0)
+
         self.__call_on_change()
 
         for neighbor in self.graph[vertex]:
@@ -31,6 +38,8 @@ class DepthFirstSearch:
                 self.visit(neighbor)
 
         self.states[vertex] = VertexState.VISITED
+        self.time += 1
+        self.times[vertex] = (self.times[vertex][0], self.time)
         self.__call_on_change()
 
     def on_change(self, callback):
