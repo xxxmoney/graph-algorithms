9a43e90abbcae6a2dd786a3f06c7557a6069310a
Jakub Hána <61245332+xxxmoney@users.noreply.github.com>
2024-05-25 11:11:08 +0200

    Fixes (#3)

--- bfs.py
+++ bfs.py
@@ -1,6 +1,7 @@
 ﻿from collections import deque
 from print_graph import print_graph
 
+
 def bfs(graph, node, positions=None):
     visited = set()
     queue = deque()
@@ -23,30 +24,3 @@ def bfs(graph, node, positions=None):
                 print_graph(graph, queue, visited, positions)
                 print('Queue: ', queue)
                 print('Visited: ', visited)
-
-
-graph = {
-    'A': ['B', 'G'],
-    'B': ['C', 'D', 'E'],
-    'C': [],
-    'D': [],
-    'E': ['F'],
-    'F': [],
-    'G': ['H'],
-    'H': ['I'],
-    'I': []
-}
-
-positions = {
-    'A': (0, 0),
-    'B': (-1, -1),
-    'C': (-2, -2),
-    'D': (-1, -2),
-    'E': (0, -2),
-    'F': (1, -2),
-    'G': (1, -1),
-    'H': (2, -1),
-    'I': (3, -1)
-}
-
-bfs(graph, 'A', positions=positions)
--- main.py
+++ main.py
@@ -4,6 +4,25 @@ from print_graph import print_graph_from_dfs
 from calculate_positions import calculate_positions
 from continous_components import ContinuousComponents
 from transpose import transpose
+from bfs import bfs
+
+
+def simple_bfs_test():
+    graph = {
+        'A': ['B', 'G'],
+        'B': ['C', 'D', 'E'],
+        'C': [],
+        'D': [],
+        'E': ['F'],
+        'F': [],
+        'G': ['H'],
+        'H': ['I'],
+        'I': []
+    }
+
+    positions = calculate_positions(graph)
+
+    bfs(graph, 'A', positions)
 
 
 def simple_dfs_test():
@@ -22,6 +41,10 @@ def simple_dfs_test():
     dfs = DepthFirstSearch(graph)
     dfs.on_change(lambda: print_graph_from_dfs(graph, dfs))
     dfs.run()
+
+    print("Times: ", dfs.times)
+    # Print second value of times tuple ordered by descending
+    print("Vertices ordered by descending: ", sorted(dfs.times, key=lambda x: dfs.times[x][1], reverse=True))
 
 
 def transpose_test():
@@ -61,10 +84,11 @@ def continuous_components_test():
 
     print("Edges: ", cc.dfs_transposed.edges)
     print("Strongly Continous Components: ", sccs)
-    
+
     print_graph_from_dfs(graph, cc.dfs_transposed)
 
 
+# simple_bfs_test()
 # simple_dfs_test()
 # transpose_test()
 continuous_components_test()
